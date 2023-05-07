01f305a9b106eb04b1acdfa08f74a03b135aed70
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-07 16:11:37 +0300

    update Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,32 @@
+import Link from 'next/link'
 import Image from 'next/image'
 
 import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      Hello World !
+    <div className={styles.promo}>
+      <div className={styles.hamburger}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.subTitle}>
+          My name is Oleksandr Khozhainov
+        </div>
+        <h1 className={styles.title}>
+          I am a React Developer <br /> from Odessa
+        </h1>
+        <div className={styles.btnWrapper}>
+          <Link href="" className={styles.btnLinkColored}>
+            Portfolio
+          </Link>
+          <Link href="" className={styles.btnLink}>
+            About me
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
