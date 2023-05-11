c489f80d79d7d79f21ed874d558b64e580d93e51
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-11 22:29:36 +0300

    add Container component and add link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,37 @@
 import Link from 'next/link'
 import { Menu } from '@/components/Menu';
 import { SocialNetworkSidebar } from '@/components/SocialNetworkSidebar';
+import { Container } from '@/components/Container';
 import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.promo}>
       <Menu />
-      <div className={styles.container}>
-        <div className={styles.subTitle}>
-          My name is Oleksandr Khozhainov
-        </div>
-        <h1 className={styles.title}>
-          I am a React Developer <br /> from Odessa
-        </h1>
-        <div className={styles.btnWrapper}>
-          <Link href="" className={styles.btnLinkColored}>
-            Portfolio
-          </Link>
-          <Link href="" className={styles.btnLink}>
-            About me
-          </Link>
-        </div>
-      </div>
+      <Container>
+        <>
+          <div className={styles.subTitle}>
+            My name is Oleksandr Khozhainov
+          </div>
+          <h1 className={styles.title}>
+            I am a React Developer <br /> from Odessa
+          </h1>
+          <div className={styles.btnWrapper}>
+            <Link
+              className={styles.btnLinkColored}
+              href='/portfolio'
+            >
+              Portfolio
+            </Link>
+            <Link
+              className={styles.btnLink}
+              href='/about-me'
+            >
+              About me
+            </Link>
+          </div>
+        </>
+      </Container>
       <SocialNetworkSidebar />
     </div>
   )
