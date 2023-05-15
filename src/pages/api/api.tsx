e8d7b96f540902636e268861e473623c85b889dc
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-15 17:20:33 +0300

    add timerData

--- src/pages/api/api.tsx
+++ src/pages/api/api.tsx
@@ -2,6 +2,7 @@ import {
   SocialNetworkData,
   AboutSkillsData,
   MenuData,
+  TimerData,
   EducationData,
 } from './interface';
 
@@ -10,7 +11,7 @@ import instagramImg from '@/icon/social/instagram.svg';
 import githubImg from '@/icon/social/github.svg';
 
 import { MdAnchor } from "react-icons/md";
-import { GrCertificate } from "react-icons/gr";
+import { GiDiploma } from "react-icons/gi";
 import { IoSchoolOutline, IoFastFoodOutline } from "react-icons/io5";
 
 // ! --- --- --- ---
@@ -95,6 +96,15 @@ export const socialNetworkData: SocialNetworkData[] = [
   },
 ];
 
+export const timerData: TimerData[] = [
+  { timeValue: 18, time: 'years' },
+  { timeValue: 11, time: 'months' },
+  { timeValue: 12, time: 'days' },
+  { timeValue: 20, time: 'hours' },
+  { timeValue: 52, time: 'minutes' },
+  { timeValue: 28, time: 'seconds' },
+]
+
 export const educationData: EducationData[] = [
   {
     mainTitle: 'IT Education',
@@ -103,13 +113,13 @@ export const educationData: EducationData[] = [
         title: 'Mate Academy',
         subTitle: 'Full day education to Front-End Developer (2022)',
         description: 'I studied at Mate Academy. The lessons lasted 4 months, 5 days a week from 9 to 18 + Conversation Club in English. A lot of practice in writing code during the study.',
-        icon: <IoSchoolOutline size={30} />,
+        icon: <IoSchoolOutline size={26} />,
       },
       {
         title: 'Hillel School',
         subTitle: 'Front-End Basic (2019) | Front-End Pro (2020)',
         description: 'I finished at Hillel School, completed 2 courses "Front-End Basic" and "Front-End Pro". Studied such technologies HTML, CSS, SCSS, Bootstrap, JavaScript, LocalStorage, Fetch requests and worked with API, Gulp, and jQuery library.',
-        icon: <GrCertificate size={30} />,
+        icon: <GiDiploma size={26} />,
       },
     ]
   },
@@ -120,13 +130,13 @@ export const educationData: EducationData[] = [
         title: 'Odessa National Maritime Academy',
         subTitle: 'Navigator | Odessa (2011-2016)',
         description: 'I finished studying at the Odessa National Maritime Academy in Odessa with the diploma of Specialist. The period of study at the Academy was 5 years.',
-        icon: <MdAnchor size={30} />,
+        icon: <MdAnchor size={26} />,
       },
       {
         title: 'ONA of Nutrition Technologies',
         subTitle: 'Restaurant business administrator | Odessa (2006-2010)',
         description: 'I finished studying at the Odessa National Academy of Nutrition Technologies in Odessa with the diploma of Bachelor. The period of study at the Academy was 4 years.',
-        icon: <IoFastFoodOutline size={30} />,
+        icon: <IoFastFoodOutline size={26} />,
       },
     ]
   },
