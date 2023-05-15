eb80cd2fb03ceb5be970c6235106e61cbf2206e7
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-15 17:20:06 +0300

    update timer section

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,8 +1,10 @@
-import { educationData } from './api';
+import { educationData, timerData } from './api';
 import { Menu } from '@/components/Menu';
 import { SocialNetworkSidebar } from '@/components/SocialNetworkSidebar';
 import { Container } from '@/components/Container';
 import styles from '@/styles/Education.module.scss'
+
+
 
 export default function Education() {
   return (
@@ -11,11 +13,8 @@ export default function Education() {
       <Container>
         <>
           <h1 className={styles.mainTitle}>
-            Education
+            My Education
           </h1>
-          <h2 className={styles.subTitle}>
-            My Education and Certificate
-          </h2>
           <div className={styles.divider} />
 
           <div className={styles.timer}>
@@ -23,24 +22,14 @@ export default function Education() {
               Experience Time
             </div>
             <div className={styles.timerWrapper}>
-              <div className={styles.timerBlock}>
-                <span id="years">03</span>year
-              </div>
-              <div className={styles.timerBlock}>
-                <span id="months">07</span>months
-              </div>
-              <div className={styles.timerBlock}>
-                <span id="days">12</span>days
-              </div>
-              <div className={styles.timerBlock}>
-                <span id="hours">20</span>hours
-              </div>
-              <div className={styles.timerBlock}>
-                <span id="minutes">56</span>minutes
-              </div>
-              <div className={styles.timerBlock}>
-                <span id="seconds">20</span>seconds
-              </div>
+              {timerData.map((data, i) => (
+                <div key={i} className={styles.timerBlock}>
+                  <span>
+                    {data.timeValue}
+                  </span>
+                  {data.time}
+                </div>
+              ))}
             </div>
           </div>
 
