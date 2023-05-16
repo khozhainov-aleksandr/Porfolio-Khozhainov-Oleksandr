db0dc31eb83e91c8d7246a974e36c45295138694
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-16 13:22:27 +0300

    add logic in timer

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,12 +1,46 @@
-import { educationData, timerData } from './api';
+import { useEffect, useState } from 'react';
+import { TimerData } from './api/interface';
+import { educationData } from './api';
 import { Menu } from '@/components/Menu';
 import { SocialNetworkSidebar } from '@/components/SocialNetworkSidebar';
 import { Container } from '@/components/Container';
 import styles from '@/styles/Education.module.scss'
 
-
-
 export default function Education() {
+  const [years, setYears] = useState<number>(2);
+  const [months, setMonths] = useState<number>(8);
+  const [days, setDays] = useState<number>(21);
+  const [hours, setHours] = useState<number>(14);
+  const [minutes, setMinutes] = useState<number>(46);
+  const [seconds, setSeconds] = useState<number>(39);
+
+  const timerData: TimerData[] = [
+    { timeValue: years, timeName: 'years' },
+    { timeValue: months, timeName: 'months' },
+    { timeValue: days, timeName: 'days' },
+    { timeValue: hours, timeName: 'hours' },
+    { timeValue: minutes, timeName: 'minutes' },
+    { timeValue: seconds, timeName: 'seconds' },
+  ]
+
+  const startTime = '2019-07-15';
+
+  const getTime = () => {
+    const time = Date.parse(startTime) - Date.now();
+
+    setYears(-Math.floor((time / (1000 * 60 * 60 * 24 * 31 * 12)) + 1));
+    setMonths(-Math.floor((time / (1000 * 60 * 60 * 24 * 31)) % 12));
+    setDays(-Math.floor((time / (1000 * 60 * 60 * 24)) % 30));
+    setHours(-Math.floor((time / (1000 * 60 * 60)) % 24));
+    setMinutes(-Math.floor((time / 1000 / 60) % 60));
+    setSeconds(-Math.floor((time / 1000) % 60));
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => getTime(), 1000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className={styles.education}>
       <Menu />
@@ -22,14 +56,17 @@ export default function Education() {
               Experience Time
             </div>
             <div className={styles.timerWrapper}>
-              {timerData.map((data, i) => (
-                <div key={i} className={styles.timerBlock}>
-                  <span>
-                    {data.timeValue}
-                  </span>
-                  {data.time}
+              {timerData.map((data, i) => {
+                const { timeValue, timeName } = data;
+                return (
+                  <div key={i} className={styles.timerBlock}>
+                    <span>
+                      {timeValue < 10 ? `0${timeValue}` : timeValue}
+                    </span>
+                    {timeName}
                 </div>
-              ))}
+                )
+              })}
             </div>
           </div>
 
