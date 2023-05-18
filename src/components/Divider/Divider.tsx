4e9236a89adc6c5a0dbfc128263ca6795b645dfe
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-18 15:12:02 +0300

    update component Divider width different position

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -1,5 +1,17 @@
+import classNames from 'classnames';
 import styles from './Divider.module.scss';
 
-export const Divider: React.FC = () => (
-  <div className={styles.divider} />
-);
+interface Props {
+  position: string;
+}
+
+export const Divider: React.FC<Props> = ({ position }) => {
+  return (
+    <div className={
+      classNames({
+        [styles.dividerStart] : position === 'start',
+        [styles.dividerCenter] : position === 'center',
+      })
+    } />
+  )
+}
