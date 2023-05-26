b5200f5cdf7f982a5da38af0ac497312021b8c26
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-26 16:31:17 +0200

    add JSX.Element[] for children

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,7 +1,7 @@
 import styles from './Container.module.scss';
 
 interface Props {
-  children: JSX.Element;
+  children: JSX.Element | JSX.Element[];
 }
 
 export const Container: React.FC<Props> = ({ children }) => (
