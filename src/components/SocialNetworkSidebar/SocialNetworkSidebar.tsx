4a47ce4d0d4d320d86a57eb0fd445485dfd98f0d
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-28 21:14:01 +0200

    update icons in SocialNetworkSidebar

--- src/components/SocialNetworkSidebar/SocialNetworkSidebar.tsx
+++ src/components/SocialNetworkSidebar/SocialNetworkSidebar.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { socialNetworkData } from '@/pages/api';
 import styles from './SocialNetworkSidebar.module.scss';
 
@@ -11,7 +12,13 @@ export const SocialNetworkSidebar: React.FC = () => (
         className={styles.link}
         target="_blank"
       >
-        {data.icon}
+        <Image
+          className={styles.icon}
+          src={data.icon}
+          alt={data.alt}
+          height={20}
+          width={20}
+        />
       </Link>
     ))}
     <div className={styles.divider}></div>
