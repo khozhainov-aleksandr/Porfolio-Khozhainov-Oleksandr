8b1c3ad83b312c2354040fec9ae009ffac27cdc6
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-29 17:26:28 +0200

    add Head and meta tag

--- src/pages/certificates.tsx
+++ src/pages/certificates.tsx
@@ -1,16 +1,15 @@
-import { useState } from 'react';
+import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState } from 'react';
 import Masonry, {ResponsiveMasonry} from 'react-responsive-masonry';
-
 import { ImagesData } from './api/interface';
-import { imagesData, certificateButtonData } from './api';
+import { metaPropertyData, imagesData, certificateButtonData } from './api';
 import { Menu } from '@/components/Menu';
 import { SocialNetworkSidebar } from '@/components/SocialNetworkSidebar';
 import { Container } from '@/components/Container';
-
-import styles from '@/styles/Certificates.module.scss'
 import { Divider } from '@/components/Divider';
+import styles from '@/styles/Certificates.module.scss'
 
 export default function Certificates() {
   const [showCertificate, setShowCertificate] = useState<ImagesData[]>(imagesData);
@@ -21,6 +20,13 @@ export default function Certificates() {
 
   return (
     <div className={styles.certificates}>
+      <Head>
+        <title>Certificates</title>
+        <meta name='title' content={metaPropertyData.siteName} />
+        <meta name='description' content={metaPropertyData.description} />
+        <meta name='keywords' content={metaPropertyData.keywords} />
+        <meta name='author' content={metaPropertyData.author} />
+      </Head>
       <Menu />
       <Container>
         <h1 className={styles.mainTitle}>
