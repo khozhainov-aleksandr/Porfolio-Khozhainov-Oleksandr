c365a33980044070ca7d5451c60d120f0e993921
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-29 12:40:45 +0200

    add Head and meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
+import Head from 'next/head';
 import Link from 'next/link'
 import { Menu } from '@/components/Menu';
 import { SocialNetworkSidebar } from '@/components/SocialNetworkSidebar';
 import { Container } from '@/components/Container';
+import metaImg from '@/img/meta/pic_meta.png';
 import styles from '@/styles/Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.promo}>
+      <Head>
+        <title>Portfolio</title>
+        <meta name='title' content='Portfolio: Khozhainov Oleksandr' />
+        <meta name='description' content='My Education and Certificate, Skills, which I use in my Projects, Works and Contacts.' />
+        <meta property='type' content='website' />
+        <meta property='url' content='https://porfolio-khozhainov-oleksandr.vercel.app' />
+        <meta property='image' content={metaImg.src} />
+      </Head>
       <Menu />
       <Container>
         <>
