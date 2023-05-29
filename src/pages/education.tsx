68bacaa48d203b6bb97fa8e0dea37c799c0f6e5f
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-29 17:26:34 +0200

    add Head and meta tag

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,6 +1,7 @@
+import Head from 'next/head';
 import { useEffect, useState } from 'react';
 import { TimerData } from './api/interface';
-import { educationData } from './api';
+import { metaPropertyData, educationData } from './api';
 import { Menu } from '@/components/Menu';
 import { SocialNetworkSidebar } from '@/components/SocialNetworkSidebar';
 import { Container } from '@/components/Container';
@@ -44,65 +45,68 @@ export default function Education() {
 
   return (
     <div className={styles.education}>
+      <Head>
+        <title>Education</title>
+        <meta name='title' content={metaPropertyData.siteName} />
+        <meta name='description' content={metaPropertyData.description} />
+        <meta name='keywords' content={metaPropertyData.keywords} />
+        <meta name='author' content={metaPropertyData.author} />
+      </Head>
       <Menu />
       <Container>
-        <>
-          <h1 className={styles.mainTitle}>
-            My Education
-          </h1>
-          <Divider position='center' />
-
-          <div className={styles.timer}>
-            <div className={styles.timerTitle}>
-              Experience Time
-            </div>
-            <div className={styles.timerWrapper}>
-              {timerData.map((data, i) => {
-                const { timeValue, timeName } = data;
-                return (
-                  <div key={i} className={styles.timerBlock}>
-                    <span>
-                      {timeValue < 10 ? `0${timeValue}` : timeValue}
-                    </span>
-                    {timeName}
-                </div>
-                )
-              })}
-            </div>
+        <h1 className={styles.mainTitle}>
+          My Education
+        </h1>
+        <Divider position='center' />
+        <div className={styles.timer}>
+          <div className={styles.timerTitle}>
+            Experience Time
           </div>
-
-          <div className={styles.educationWrapper}>
-            {educationData.map((data, i) => (
-              <div key={i} className={styles.educationColumn}>
-                <h2 className={styles.educationTitle}>
-                  {data.mainTitle}
-                </h2>
-                <ul>
-                  {data.items.map((item, i) => (
-                    <li key={i}>
-                      <div className={styles.educationItem}>
-                        <div className={styles.educationItemHead}>
-                          <div className={styles.educationItemIcon}>
-                            {item.icon}
-                          </div>
-                          <h4 className={styles.educationItemTitle}>
-                            {item.title}
-                          </h4>
-                          <div className={styles.educationItemLocation}>
-                            {item.subTitle}
-                          </div>
-                        </div>
-                        <p className={styles.educationItemDescription}>
-                          {item.description}
-                        </p>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
+          <div className={styles.timerWrapper}>
+            {timerData.map((data, i) => {
+              const { timeValue, timeName } = data;
+              return (
+                <div key={i} className={styles.timerBlock}>
+                  <span>
+                    {timeValue < 10 ? `0${timeValue}` : timeValue}
+                  </span>
+                  {timeName}
               </div>
-            ))}
+              )
+            })}
           </div>
-        </>
+        </div>
+        <div className={styles.educationWrapper}>
+          {educationData.map((data, i) => (
+            <div key={i} className={styles.educationColumn}>
+              <h2 className={styles.educationTitle}>
+                {data.mainTitle}
+              </h2>
+              <ul>
+                {data.items.map((item, i) => (
+                  <li key={i}>
+                    <div className={styles.educationItem}>
+                      <div className={styles.educationItemHead}>
+                        <div className={styles.educationItemIcon}>
+                          {item.icon}
+                        </div>
+                        <h4 className={styles.educationItemTitle}>
+                          {item.title}
+                        </h4>
+                        <div className={styles.educationItemLocation}>
+                          {item.subTitle}
+                        </div>
+                      </div>
+                      <p className={styles.educationItemDescription}>
+                        {item.description}
+                      </p>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </Container>
       <SocialNetworkSidebar />
     </div>
