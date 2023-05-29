56a76b6ebc7f5a4436e1e96b74f32fd363ec6f99
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-29 17:26:44 +0200

    add Head and meta tag

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,25 +1,29 @@
+import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
-
-import { portfolioData } from './api';
+import { metaPropertyData, portfolioData } from './api';
 import { Menu } from '@/components/Menu';
 import { SocialNetworkSidebar } from '@/components/SocialNetworkSidebar';
 import { Container } from '@/components/Container';
 import { Divider } from '@/components/Divider';
-
-import styles from '@/styles/Portfolio.module.scss'
+import styles from '@/styles/Portfolio.module.scss';
 
 export default function Portfolio() {
   return (
     <div className={styles.portfolio}>
+      <Head>
+        <title>Portfolio</title>
+        <meta name='title' content={metaPropertyData.siteName} />
+        <meta name='description' content={metaPropertyData.description} />
+        <meta name='keywords' content={metaPropertyData.keywords} />
+        <meta name='author' content={metaPropertyData.author} />
+      </Head>
       <Menu />
       <Container>
         <h1 className={styles.mainTitle}>
           My Portfolio
         </h1>
         <Divider position='center' />
-
-
         <div className={styles.linkWrapper}>
           {portfolioData.map((data, i) => (
             <Link
