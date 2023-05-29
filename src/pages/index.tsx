50d5b5536073d472abd69a18da1aa25852c65a51
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2023-05-29 13:53:54 +0200

    remove test commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,12 @@ export default function Home() {
       <Head>
         <title>{metaPropertyData.title}</title>
         <meta name='title' content={metaPropertyData.siteName} />
-        <meta name='description' content='My Education and Certificate, Skills, which I use in my Projects, Works and Contacts.' />
+        <meta name='description' content={metaPropertyData.description} />
         <meta name='keywords' content={metaPropertyData.keywords} />
         <meta name='author' content={metaPropertyData.author} />
 
         <meta property="og:title" content={metaPropertyData.siteName} />
-        <meta property="og:description" content='My Education and Certificate, Skills, which I use in my Projects, Works and Contacts.' />
+        <meta property="og:description" content={metaPropertyData.description} />
         <meta property="og:type" content={metaPropertyData.type} />
         <meta property="og:url" content={metaPropertyData.url} />
         <meta property="og:image" content={metaPropertyData.image} />
